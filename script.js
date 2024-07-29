40785c3aafb128d975dd9e6642cfe2bc18d86c52
Min <150890227+akkm9120@users.noreply.github.com>
2024-07-30 06:10:43 +0800

    final commit | minn

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
-document.getElementById('toggle-button').addEventListener('click', function() {
-    var navbarLinks = document.getElementById('navbar-links');
-    if (navbarLinks.classList.contains('active')) {
-        navbarLinks.classList.remove('active');
-    } else {
-        navbarLinks.classList.add('active');
-    }
+// document.getElementById('toggle-button').addEventListener('click', function() {
+//     var navbarLinks = document.getElementById('navbar-links');
+//     if (navbarLinks.classList.contains('active')) {
+//         navbarLinks.classList.remove('active');
+//     } else {
+//         navbarLinks.classList.add('active');
+//     }
+// });
+
+const navLinks = document.querySelectorAll(".navbar-nav .custom-link");
+const cardCategories = document.querySelectorAll(".card-category");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const category = this.id.replace("-favourites", "-cards");
+    cardCategories.forEach((cards) => {
+      if (cards.id === category) {
+        cards.style.display = "flex";
+      } else {
+        cards.style.display = "none";
+      }
+    });
+  });
 });
 
-   
-
+const navSelect = document.getElementById("navSelect");
+navSelect.addEventListener("change", function () {
+  const selectedCategory = this.value;
+  cardCategories.forEach((cards) => {
+    if (cards.id === selectedCategory) {
+      cards.style.display = "flex";
+    } else {
+      cards.style.display = "none";
+    }
+  });
+});
